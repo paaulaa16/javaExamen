f9489b31677dbfbbbd79482e47c64d298958cebe
Paula Caballero <paaulacaballero@gmail.com>
2021-01-17 11:46:00 +0100

    first commit

---
+++ README.md
@@ -0,0 +1,8 @@
+# Plantilla JAVA par JUnit5
+
+Plantilla para MSCode.
+
+Incluye la librería junit en lib.
+Incluye una plantilla de test para unas clase genéricas Calculadora 
+
+para 1DAW3 en Plaiaundi
---
+++ src/App.java
@@ -0,0 +1,13 @@
+import app.Fraccion;
+
+public class App {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Hello, World!");
+
+        Fraccion f1 = new Fraccion(5, 10);
+        Fraccion f2 = new Fraccion(7, 20);
+        System.out.println(f1);
+        System.out.println(f2);
+
+    }
+}
